42f6b4488c6edfc4ab7478735ce4fb10e9f460f7
cunhagdc <gustavocunhaads@gmail.com>
2025-04-09 10:08:59 -0300

    Alteração login

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,10 @@ function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/spreadsheet" element={<Spreadsheet />} />
         <Route path="/login" element={<Login />} />
-=======
-        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to="/dashboard" replace />} />
-        <Route path="/" element={isAuthenticated ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />} />
-        <Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" replace />} />
-        <Route path="/spreadsheet" element={isAuthenticated ? <Spreadsheet /> : <Navigate to="/login" replace />} />
->>>>>>> 490d8ae6ea31a7ebca394bbb6d7e291643d2814e
       </Routes>
     </Router>
   );
